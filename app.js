51a919d47482020a29d49992c2ec9c5e3d6a1224
AntonEmery <antonemery@gmail.com>
2015-06-22 21:37:18 -0700

    basic questions array

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-$('#quiz').submit(function(){
-	alert('Question submitted');
+//store each question in an array within a larger array
+//user selects question and submits. it takes the ID of that radio button and checks it against the answer id in that specific question array
 
-})
+var questions = [['question one', 'answer one'], ['question two', 'answer two']];
+
+//test function to check form submission
+function quiz() {
+	alert('submitted');
+}
