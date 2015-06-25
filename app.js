0ea67483831c3b792a448b1a39363be3ea6649ea
AntonEmery <antonemery@gmail.com>
2015-06-24 18:12:05 -0700

    alerts if answer is correct

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 //store each question in an array within a larger array
 //user selects question and submits. it takes the ID of that radio button and checks it against the answer id in that specific question array
 
-var questions = [['question one', 'answer one'], ['question two', 'answer two']];
+var questions = [['What color is an apple?', 1], ['question two', 'answer two']];
 
-//when this function is called need to log the value of the answer the user chose
+// //when this function is called need to log the value of the answer the user chose
 function submitAnswer() {
-	//console log value of selected radio button	
-	alert($('input[name="answer"]:checked').val());
+	var answer = ($('input[name="answer"]:checked').val());
+	if(answer == questions[0][1]) {
+		alert('That is correct!');
+	} else {
+		alert('Try again!');
+	}
 }
+
