195b52ae72036981285dc9f99ad6f30500770df7
AntonEmery <antonemery@gmail.com>
2015-06-25 19:54:17 -0700

    store questions in array of objects

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //store each question in an array within a larger array
 //user selects question and submits. it takes the ID of that radio button and checks it against the answer id in that specific question array
 
-var questions = [['What color is an apple?', 1], ['question two', 'answer two']];
+var allQuestions = [{question: "What color is an apple?", choices: ["red", "purple", "									blue"], correctAnswer: 0}, 
+									 {question: "Java and Javascript are the same language", choices: ["true", "false"], correctAnswer: 0},
+									 {question: "What is the capital of Oregon?", choices: ["Portland", "Salem", "Bend"], correctAnswer: 1}];
 
 // //when this function is called need to log the value of the answer the user chose
 function submitAnswer() {
