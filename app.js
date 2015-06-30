c54b7b8c409fb260d827354c3661e8d5b7e9b00c
AntonEmery <antonemery@gmail.com>
2015-06-29 21:51:11 -0700

    need to increment current question num

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 //store each question in an array within a larger array
 //user selects question and submits. it takes the ID of that radio button and checks it against the answer id in that specific question array
 
-var allQuestions = [{question: "What color is an apple?", choices: ["red", "purple", "									blue"], correctAnswer: 0}, 
-									 {question: "Java and Javascript are the same language", choices: ["true", "false"], correctAnswer: 0},
-									 {question: "What is the capital of Oregon?", choices: ["Portland", "Salem", "Bend"], correctAnswer: 1}];
+var allQuestions = [{question: "What color is an apple?", choices: ["red", "purple", "										blue"], correctAnswer: 0}, 
+									 {question: "Javascript is a language primarily used in the", choices: ["web browser", "server", "personal computer"], correctAnswer: 0},
+									 {question: "What is the capital of Oregon?", choices: ["Portland", "Salem", "Bend"], correctAnswer: 1},
+									 {question: "What is a common nickname for Portland?", choices:["Hiptown", "Stumptown", "Jewel of the North West"], correctAnswer: 1},
+									 {question: "Portland is known for its:", choices: ["coffee", "hipsters", "all of the above"], correctAnswer: 2}];
 
 //compares the correct answer with the submitted answer
 function submitAnswer() {
 	var currentQuestion = getQuestionNumber();
 	var selectedAnswer = ($('input[name="answer"]:checked').val());
 	correctAnswer(currentQuestion, selectedAnswer);
+	populateQuestion();
 	} 
-
-
-
-//determines correct answer
-function correctAnswer (questionNumber, selectedAnswer) {
-	var correctAnswer = allQuestions[questionNumber - 1].correctAnswer + 1;
-	if(correctAnswer == selectedAnswer) {
-		alert('correct');
-	} else {
-		alert('wrong');
-	}
-}
 
 //gets question number
 function getQuestionNumber() {
@@ -30,23 +21,38 @@ function getQuestionNumber() {
 	return formID;
 	}
 
-//populates the next question 
+var finalScore = 0;
+
+//determines correct answer
+function correctAnswer (questionNumber, selectedAnswer) {
+	var correctAnswer = allQuestions[questionNumber - 1].correctAnswer + 1;
+	if(correctAnswer == selectedAnswer) {
+		alert('correct');
+		$("form").attr("id", currentQuestion + 1)
+		finalScore++
+	} else {
+		alert('wrong');
+	}
+}
+
+
+//populates the question 
 var questionOnPage = 0;
 
 function populateQuestion() {
-		$('h3').text(allQuestions[questionOnPage].question);
-		questionOnPage++;
-	}
-
-
-//loop through choices array
+	var questionContent = allQuestions[questionOnPage].question;
+	$('h3').text(questionContent);
+	//loop through choices array
 	//select input tag equal to counter in loop
 	//after input tag add that corresponding item in array
 	for(var i = 0; i < allQuestions[questionOnPage].choices.length; i++) {
-	$('input:eq('+ i +')').after(allQuestions[questionOnPage].choices[i]);
+		//$('input:eq('+ i +')').after(allQuestions[questionOnPage].choices[i]);
+		$('input:eq('+ i +')')[0].nextSibling.nodeValue = allQuestions[questionOnPage].choices[i];
+	}
+	questionOnPage++;
 }
 
-populateQuestion();
+
 
 
 
